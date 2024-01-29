04872c544c8ef03af0f38194ede02ab949bb81cb
mnrniloy <mnrniloy0@gmail.com>
2024-01-29 11:04:24 +0600

    guacamole: Update Dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,7 +13,7 @@ git clone https://github.com/mnrniloy/hardware_oneplus.git -b 13 hardware/oneplu
 echo ""
 
 echo "Cloning Vendor blobs"
-git clone https://github.com/mnrniloy/vendor_oneplus_guacamole.git -b 14 vendor/oneplus/guacamole
+git clone https://github.com/mnrniloy/vendor_oneplus_guacamole.git vendor/oneplus/guacamole
 git clone https://github.com/mnrniloy/vendor_oneplus_sm8150-common.git -b 13 vendor/oneplus/sm8150-common
 echo ""
 
@@ -29,5 +29,32 @@ echo ""
 echo "Cloning Apps"
 git clone https://github.com/KProfiles/android_packages_apps_Kprofiles packages/apps/Kprofiles
 git clone https://gitlab.com/nrniloy369/vendor_oneplus_apps.git vendor/oneplus/apps
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 echo ""
+
+#  Firmwares
+# URL of the ZIP file
+zip_url="https://sourceforge.net/projects/afterlife-projects/files/release/Op7/firmware_oneplus7pro.zip/download"
+
+# Destination directory for extraction
+extract_dir="vendor/oneplus/guacamole/"
+extract_dir_check="vendor/oneplus/guacamole/radio"
+
+# Check if files already exist in the destination directory
+if [ -e "$extract_dir_check/modem.img" ] && [ -e "$extract_dir_check/abl.img" ]; then
+    echo "Firmware already exist. No need to download."
+else
+    # Download the ZIP file
+    curl -LJO "$zip_url"
+
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "Firmware Download successful. Extracting..."
+        # Extract the ZIP file
+        unzip -q "*.zip" -d "$extract_dir"
+        echo "Firmware Extraction complete."
+        # Optional: Remove the downloaded ZIP file
+        rm *.zip
+    else
+        echo "Firmware Download failed."
+    fi
+fi
